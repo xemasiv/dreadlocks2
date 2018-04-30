5178b47b8b0a14c391d04ed17562c68e6556bd63
xemasiv <xemasiv@gmail.com>
2018-04-30 13:59:33 +0800

    v1.0.0

---
+++ README.md
@@ -0,0 +1,111 @@
+# dreadlocks2
+Dread-free locks that drive consistency.
+
+### Summary
+
+* Create and use keys to identify objects:
+  * `let key1 = hash(anything)`
+* Create key sets for keys involved in each task:
+  * `const keySet = [key1, key2, key3]`
+* Lock these key sets
+  * `Dread.lock(keySet).then(task)`
+* Release these key sets once you're done.
+  * `Dread.release(keySet)`
+
+### Perks
+
+* Respects `FIFO` (first-in-first-out) flow:
+  * Iterates on queue item 0 to n then back to 0.
+  * Reverts index to 0 on each performed lock.
+  * Reverts index to 0 on each performed release.
+* Uses `Map` so you can use anything as a key.
+  * Although of course, use of `strings` are the most encouraged.
+* Uses `setInterval` for periodic checks and processing of queue.
+  * `new Dreadlock(interval)` accepts custom `interval` in ms, `4` by default; 1000/4 = `250 checks/s`
+
+### Use Cases:
+
+* Database consistency & transactions.
+* Ensuring order in execution of tasks that wish to modify possibly similar objects or entities.
+
+
+### Changelog
+
+* v2.x
+  * Added pre-queue check if keys can be locked immediately.
+* v1.x
+  * Basic functionality.
+
+---
+
+### class `Dreadlock`
+* constructor (`interval`)
+  * `interval` optional Integer, in ms
+* lock (`items`)
+  * `items` array of keys to lock
+  * RETURNS Promise, resolves on lock success
+* release (`items`)
+  * `items` array of keys to release
+  * RETURNS Promise, resolves on release success
+* size
+  * RETURNS current size of instance `Map`
+---
+
+### Usage:
+
+* Install:
+```
+npm install dreadlocks2 --save
+yarn add dreadlocks2
+```
+```
+const Dreadlock = require('dreadlocks2');
+```
+
+* Create instance:
+```
+const Dread = new Dreadlock();
+```
+
+* Lock your keys anywhere in your code:
+```
+const keySet1 = ['key1', 'key2', 'key3'];
+Dread.lock(keySet1)
+  .then(() => {
+    console.log('Working with keySet1.');
+    // Use your keySet1 keys here
+    console.log('Done with keySet1.');
+    return Dread.release(keySet1);
+  })
+  .then(() => {
+    console.log('keySet1 released.');
+  });
+```
+```
+const keySet2 = ['key1', 'key4', 'key5'];
+Dread.lock(keySet2)
+  .then(() => {
+    console.log('Working with keySet2.');
+    // Use your keySet2 keys here
+    console.log('Done with keySet2.');
+    return Dread.release(keySet2);
+  })
+  .then(() => {
+    console.log('keySet2 released.');
+  });
+```
+* See the magic:
+```
+Working with keySet1.
+Done with keySet1.
+keySet1 released.
+Working with keySet2.
+Done with keySet2.
+keySet2 released.
+```
+
+---
+
+### LICENSE
+
+![C](https://upload.wikimedia.org/wikipedia/commons/8/84/Public_Domain_Mark_button.svg) ![0](https://upload.wikimedia.org/wikipedia/commons/6/69/CC0_button.svg)
---
+++ index.js
@@ -0,0 +1,91 @@
+class Dreadlock {
+  constructor(interval) {
+    this._lock = new Map();
+    this._queue = [];
+    this._processing = false;
+    this._interval = (
+      Boolean(interval) === true &&
+      typeof interval === 'number' &&
+      parseInt(interval) > 0
+    ) ? interval : 4;
+  }
+  lock(items) {
+    let dread = this;
+    let p = new Promise((resolve) => {
+      if (items.every((item) => dread._lock.has(item) === false) === true) {
+        items.map((item) => dread._lock.set(item));
+        resolve();
+      } else {
+        dread._queue.push({
+          items,
+          resolve
+        });
+        // console.log('PUSH', items);
+        if (Boolean(dread.timeout) === false) {
+          // console.log('timeout not found, creating');
+          dread.index = 0;
+          dread.timeout = setInterval(() => {
+            // console.log('timeout called');
+            // console.log(dread.index, dread._queue.length, dread.index >= dread._queue.length);
+            if (dread.index >= dread._queue.length) {
+              // console.log('invalid dread.index, back to zero');
+              dread.index = 0;
+            }
+            // console.log('checking', dread.index);
+            let selected = dread._queue[dread.index];
+            let {
+              items
+            } = selected;
+            // console.log('current items', items);
+            if (items.every((item) => dread._lock.has(item) === false) === true) {
+              let {
+                resolve
+              } = selected;
+              // console.log('current resolve', resolve);
+              // console.log('locking', items);
+              items.map((item) => dread._lock.set(item));
+              // console.log('lock is now', dread._lock);
+              resolve();
+              // console.log('removing from queue', items);
+              dread._queue.splice(dread.index, 1);
+              // console.log('queue is now', dread._queue);
+              // console.log('queue length is now', dread._queue.length);
+              if (dread._queue.length > 0) {
+                // console.log('queue still not empty, back to zero');
+                dread.index = 0;
+              } else {
+                // console.log('queue now empty, clearing timeout');
+                clearInterval(dread.timeout);
+                delete dread.timeout;
+              }
+            } else {
+              // console.log('conflict found, cant lock', items, 'see', Array.from(dread._lock.keys()));
+              if (dread._queue.length > 0) {
+                // console.log('from', dread.index);
+                dread.index = dread.index + 1;
+                // console.log('moving to next item', dread.index);
+              }
+            }
+
+          }, dread._interval);
+        }
+      }
+    });
+    return p;
+  }
+  release(items) {
+    let dread = this;
+    // console.log('releasing items', items);
+    let p = new Promise((resolve) => {
+      items.map((item) => dread._lock.delete(item));
+      // console.log('reverting index to zero upon release');
+      dread.index = 0;
+      resolve();
+    });
+    return p;
+  }
+  get size() {
+    return this._lock.size;
+  }
+}
+module.exports = Dreadlock;
